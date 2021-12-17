e47c578b80c84d4c0b0346d375acc1cc680d085a
Palak Rai <palakrai0108@bitbucket.org>
2021-12-17 16:39:29 +0530

    initial comit

---
+++ sales_project/sales_app/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_price = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+    def __str__(self):
+        return self.product_name
+
+class Sales(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    sale_date = models.DateField()
+    product_quantity = models.IntegerField(default=0)
+    unit_price = models.DecimalField(decimal_places=2, max_digits=10)
+    total_price = models.IntegerField()
+
